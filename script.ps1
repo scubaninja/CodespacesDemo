5a692b136fcaa00bcf962f2e56548d8de7ff68dc
April Edwards <april.edwards@microsoft.com>
2021-02-24 16:50:11 +0000

    removing script

--- script.ps1
+++
@@ -1,9 +0,0 @@
-$resourceGroup = "RG-StorageEUW"
-$location = "westeurope"
-New-AzResourceGroup -Name $resourceGroup -Location $location
-
-New-AzStorageAccount -ResourceGroupName $resourceGroup `
-  -Name <account-name> `
-  -Location $location `
-  -SkuName Standard_RAGRS `
-  -Kind StorageV2
